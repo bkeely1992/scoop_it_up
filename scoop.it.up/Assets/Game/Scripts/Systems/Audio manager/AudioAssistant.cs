887050fad235c716ca8d0b49a8dacb10e7e8eb4f
Keely Brown <bkeely1992@gmail.com>
2020-04-20 07:48:39 -0400

    Initialization of the MenuManager, singleton, AudioManager, SceneManager and Addressables.

---
+++ scoop.it.up/Assets/Game/Scripts/Systems/Audio manager/AudioAssistant.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioAssistant : MonoBehaviour
+{
+	public enum AudioType
+	{
+		SoundFx,
+		Background
+	};
+	public AudioType audioType = AudioType.SoundFx;
+
+	private AudioSource audioSource;
+
+	private void Awake()
+	{
+		audioSource = GetComponent<AudioSource>();
+		switch(audioType)
+		{
+			case AudioType.Background:
+				AudioManager.Instance.onBackgroundVolumeChanged.AddListener(onVolumeChanged);
+				audioSource.volume = AudioManager.Instance.backgroundVolume;
+				break;
+
+			case AudioType.SoundFx:
+				AudioManager.Instance.onSoundFxVolumeChanged.AddListener(onVolumeChanged);
+				audioSource.volume = AudioManager.Instance.soundFxVolume;
+				break;
+		}
+	}
+
+	private void OnDestroy()
+	{
+		switch (audioType)
+		{
+			case AudioType.Background:
+				AudioManager.Instance.onBackgroundVolumeChanged.RemoveListener(onVolumeChanged);
+				break;
+
+			case AudioType.SoundFx:
+				AudioManager.Instance.onSoundFxVolumeChanged.RemoveListener(onVolumeChanged);
+				break;
+		}
+	}
+
+	void onVolumeChanged(float volume)
+	{
+		audioSource.volume = volume;
+	}
+}
---
+++ scoop.it.up/Assets/Game/Scripts/Systems/Audio manager/AudioManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AudioManager : Singleton<AudioManager>
+{
+	public float backgroundVolume = 1.0f;
+	public float soundFxVolume = 1.0f;
+
+	[System.Serializable]
+	public class VolumeChangeEvent : UnityEvent<float> { }
+
+	[HideInInspector] public VolumeChangeEvent onBackgroundVolumeChanged = new VolumeChangeEvent();
+	[HideInInspector] public VolumeChangeEvent onSoundFxVolumeChanged = new VolumeChangeEvent();
+
+	private void Awake()
+	{
+		// Handle Loading here
+	}
+
+	public void setBackgroundVolume(float volume)
+	{
+		if (volume > 1.0f)
+		{
+			volume = 1.0f;
+		}
+		else if (volume < 0.0f)
+		{
+			volume = 0.0f;
+		}
+		backgroundVolume = volume;
+		onBackgroundVolumeChanged.Invoke(backgroundVolume);
+	}
+
+	public void setSoundFxVolume(float volume)
+	{
+		if (volume > 1.0f)
+		{
+			volume = 1.0f;
+		}
+		else if (volume < 0.0f)
+		{
+			volume = 0.0f;
+		}
+		soundFxVolume = volume;
+		onSoundFxVolumeChanged.Invoke(soundFxVolume);
+	}
+}
